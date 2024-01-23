a1a06118724fb25bc1377166cd1387e472531ad9
darklucik <rustamzonovsultan3@gmail.com>
2024-01-23 20:58:27 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import React from 'react';
+import { RegistrationPage } from './pages/registration-page/registrationPage';
+import { LoginPage } from './pages/login-page/login-page';
 
-function App() {
+const App = () => {
+  const routerConfig = createBrowserRouter (
+    [{
+      path:'/',
+      element:<RegistrationPage />,
+    },
+    {
+      path:'/login-page',
+      element: <LoginPage />
+    }
+  ]
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <RouterProvider router={routerConfig} />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/pages/login-page/login-page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { AppInput } from "../../components/appElements/appInput";
+
+export const LoginPage = () => {
+    return (
+        <div></div>
+    )
+}
